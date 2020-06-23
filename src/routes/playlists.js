e5508c3983032b2239342f65f6c3edf5775857a7
Ryan Bahan <ryan@ryanbahan.com>
2020-06-23 15:18:23 -0600

    Set up playlist GET route

--- src/routes/playlists.js
+++ src/routes/playlists.js
@@ -4,9 +4,17 @@ import { sequelize } from '../models';
 const router = Router();
 
 router.get('/:id', async (req, res) => {
-    const playlists = await sequelize.models.playlists.findAll();
-    console.log(playlists[0].dataValues)
-    return res.send(`${JSON.stringify(playlists[0].dataValues)}`);
+    const playlists = await sequelize.models.playlists.findAll({
+        where: {
+            user_id: req.params.id
+        }
+    });
+
+    if (playlists.length) {
+        return res.send(JSON.stringify(playlists))
+    } else {
+        return res.send("Nothing matched your query")
+    }
 });
 
 export default router;
