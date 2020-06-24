64502680dd008bbbf70c90e38078e937bb233e78
Ryan Bahan <ryan@ryanbahan.com>
2020-06-24 13:37:17 -0600

    Add POST request to playlists endpoint

--- src/routes/playlists.js
+++ src/routes/playlists.js
@@ -3,19 +3,33 @@ import { sequelize } from '../models';
 
 const router = Router();
 
-router.get('/:id', async (req, res) => {
-    const playlists = await sequelize.models.playlists.findAll({
-        where: {
-            user_id: req.params.id
-        },
-        include: sequelize.models.playlist_items
-    });
+router.get('/:user_id', async (req, res) => {
+    const playlists = {
+        data: await sequelize.models.playlists.findAll({
+            where: {
+                user_id: req.params.user_id
+            },
+            include: sequelize.models.playlist_items
+        })
+    };
 
-    if (playlists.length) {
+    if (playlists.data.length) {
         return res.send(JSON.stringify(playlists))
     } else {
         return res.send("Nothing matched your query")
     }
+});
+
+router.post('/:user_id', async (req, res) => {
+    const playlist = await sequelize.models.playlists.create(
+        { 
+            user_id: req.body.user_id, 
+            title: req.body.title, 
+            status: req.body.status, 
+            due_date: req.body.due_date,
+        }
+    );
+    return res.send(JSON.stringify(playlist))
 });
 
 export default router;
